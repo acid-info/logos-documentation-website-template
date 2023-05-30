736ca0980d161461012c6007f82074703368b26b
amirhouieh <amir@status.im>
2023-05-30 13:48:40 +0200

    switiching to v1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,6 +11,9 @@ const config = {
   customFields: {
     ghostAPiKey: process.env.GHOST_API_KEY,
   },
+  markdown: {
+    mermaid: true,
+  },
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -25,20 +28,135 @@ const config = {
       '@acid-info/logos-docusaurus-preset',
       /** @type {import('@acid-info/logos-docusaurus-preset').PluginOptions} */
       ({
-        businessUnit: 'Logos',
+        businessUnit: 'Codex',
         theme: {
           name: 'default',
           options: {
             customCss: [require.resolve('./src/css/custom.scss')],
           },
         },
+        // in case you do not need docs, set the docs to false
+        docs: {
+          //  change this to '/' if you do want to use docs as landing page
+          routeBasePath: '/docs',
+          // enable this to have versioning
+          // versions: {
+          //   current: {
+          //     label: 'current',
+          //   },
+          // },
+          // lastVersion: 'current',
+        },
       }),
     ],
   ],
+  themes: ['@docusaurus/theme-mermaid'],
 
   themeConfig:
     /** @type {import('@acid-info/logos-docusaurus-preset').ThemeConfig} */
-    ({}),
+    ({
+      navbar: {
+        items: [
+          {
+            label: 'Docs',
+            href: '/docs',
+          },
+          {
+            label: 'Features',
+            href: '#features',
+          },
+          {
+            label: 'Showcase',
+            href: '#showcase',
+          },
+        ],
+      },
+      footer: {
+        copyright: 'Copyright @2023 Logos <br/> Built with Docusaurus.',
+        links: [
+          {
+            title: 'Learn',
+            items: [
+              {
+                href: '/',
+                label: 'Introduction',
+              },
+              {
+                href: '/',
+                label: 'Installation',
+              },
+              {
+                href: '/',
+                label: 'Migrate from v1 to v2',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                href: '/',
+                label: 'Blog',
+              },
+              {
+                href: '/',
+                label: 'Changelog',
+              },
+              {
+                href: '/',
+                label: 'Github',
+              },
+              {
+                href: '/',
+                label: 'Twitter',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                href: '/',
+                label: 'Stack Overflow',
+              },
+              {
+                href: '/',
+                label: 'Feature Requests',
+              },
+              {
+                href: '/',
+                label: 'Discord',
+              },
+              {
+                href: '/',
+                label: 'Help',
+              },
+            ],
+          },
+          {
+            title: 'Legal',
+            items: [
+              {
+                href: '/',
+                label: 'Privacy',
+              },
+              {
+                href: '/',
+                label: 'Terms',
+              },
+              {
+                href: '/',
+                label: 'Data policy',
+              },
+              {
+                href: '/',
+                label: 'Cookie policy',
+              },
+            ],
+          },
+        ],
+      },
+    }),
 }
 
 module.exports = config
